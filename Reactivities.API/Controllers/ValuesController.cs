f8abfdc313c10255dad10b2faba13c4ae55c1d89
David Potter <destroyonload@gmail.com>
2021-01-25 21:46:30 +0800

    Initial Commit Project skeleton created and tested.

---
+++ Reactivities.API/Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Reactivities.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reactivities.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FindAsync(id);
+
+            if (value == null)
+                return NotFound();
+
+            return Ok(value);
+        }
+    }
+}
