981886eb7975f66af8752ffd7ce09ab612b39e83
David Potter <destroyonload@gmail.com>
2021-01-27 17:47:22 +0800

    Added functionality that when an activity is being edited, the activity form populates with the current data / Creating an activity still renders a blank form.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,14 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Container, Header, Icon, List } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
 import axios from "axios";
 import "semantic-ui-css/semantic.min.css";
 import { IActivity } from "../models/activity";
 import { NavBar } from "../../features/nav/NavBar";
 import { ActivityDashboard as ActivityDashboard } from "../../features/activities/dashboard/ActivityDashboard";
-
-interface IState {
-  activities: IActivity[];
-}
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
