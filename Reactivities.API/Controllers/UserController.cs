ded3ef490e8e9290ca2b0a96255cd05e57acbb36
David Potter <destroyonload@gmail.com>
2021-01-31 11:19:31 +0800

    Added initial User API controller.

---
+++ Reactivities.API/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reactivities.Application.Users;
+using Reactivities.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reactivities.API.Controllers
+{
+    public class UserController : BaseController
+    {
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(Login.Query query)
+        {
+            return await Mediator.Send(query);
+        }
+    }
+}
