e83e2bd311e40462a89a8f42ac74ba083a815e8f
David Potter <destroyonload@gmail.com>
2021-01-31 20:55:31 +0800

    Added visual display of logged in status on Hompage and NavBar.

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,9 +1,12 @@
 import { observer } from "mobx-react-lite";
-import React from "react";
-import { NavLink } from "react-router-dom";
-import { Button, Container, Menu } from "semantic-ui-react";
+import React, { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { Button, Container, Menu, Image, Dropdown } from "semantic-ui-react";
+import { RootStoreContext } from "../../app/stores/rootStore";
 
 export const NavBar = () => {
+  const rootStore = useContext(RootStoreContext);
+  const { isLoggedIn, user } = rootStore.userStore;
   return (
     <Menu fixed="top" inverted>
       <Container>
@@ -21,6 +24,26 @@ export const NavBar = () => {
             to="/createActivity"
           />
         </Menu.Item>
+        {user && (
+          <Menu.Item position="right">
+            <Image
+              avatar
+              spaced="right"
+              src={user.image || "/assets/user.png"}
+            />
+            <Dropdown pointing="top left" text={user?.displayName}>
+              <Dropdown.Menu>
+                <Dropdown.Item
+                  as={Link}
+                  to={"/profile/username"}
+                  text="My Profile"
+                  icon="user"
+                />
+                <Dropdown.Item text="Logout" icon="power" />
+              </Dropdown.Menu>
+            </Dropdown>
+          </Menu.Item>
+        )}
       </Container>
     </Menu>
   );
