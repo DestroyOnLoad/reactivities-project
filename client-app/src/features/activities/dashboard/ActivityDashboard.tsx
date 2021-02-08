22477b60961a5a61705a3669bc3cd3b4d8cdbf75
David Potter <destroyonload@gmail.com>
2021-02-08 17:21:27 +0800

    Implemented pagination on the activities dashboard with a button click. [This works but will be replaced with pagination load on scrolling event]

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,19 +1,32 @@
 import { observer } from "mobx-react-lite";
-import React, { useContext, useEffect } from "react";
-import { Grid } from "semantic-ui-react";
+import React, { useContext, useEffect, useState } from "react";
+import { Button, Grid } from "semantic-ui-react";
 import { LoadingComponent } from "../../../app/layout/LoadingComponent";
 import ActivityList from "./ActivityList";
 import { RootStoreContext } from "../../../app/stores/rootStore";
 
 const ActivityDashboard: React.FC = () => {
   const rootStore = useContext(RootStoreContext);
-  const { loadActivities, loadingIndicator } = rootStore.activityStore;
+  const {
+    loadActivities,
+    loadingIndicator,
+    setPage,
+    page,
+    totalPages,
+  } = rootStore.activityStore;
+  const [loadingNext, setLoadingNext] = useState(false);
+
+  const handleGetNextPage = () => {
+    setLoadingNext(true);
+    setPage(page + 1);
+    loadActivities().then(() => setLoadingNext(false));
+  };
 
   useEffect(() => {
     loadActivities();
   }, [loadActivities]);
 
-  if (loadingIndicator)
+  if (loadingIndicator && page === 0)
     return (
       <LoadingComponent content={"Loading activities..."}></LoadingComponent>
     );
@@ -22,6 +35,14 @@ const ActivityDashboard: React.FC = () => {
     <Grid>
       <Grid.Column width={10}>
         <ActivityList />
+        <Button
+          positive
+          floated="right"
+          content="More..."
+          onClick={handleGetNextPage}
+          loading={loadingNext}
+          disabled={totalPages === page + 1}
+        />
       </Grid.Column>
       <Grid.Column width={6}>
         <h1>Activity filters</h1>
