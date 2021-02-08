a25598834117f1a2a9c4fde5bd689248be39eab4
David Potter <destroyonload@gmail.com>
2021-02-08 18:02:24 +0800

    Updated activities list query to include additional optional filters on the back-end. Updated the controller to pass in the optional values.

--- Reactivities.Application/Activities/List.cs
+++ Reactivities.Application/Activities/List.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Reactivities.Application.Interfaces;
 using Reactivities.Persistence;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -19,36 +21,58 @@ namespace Reactivities.Application.Activities
 
         public class Query : IRequest<ActivitiesEnvelope>
         {
-            public Query(int? limit, int? offset)
+            public Query(int? limit, int? offset, bool isGoing, bool isHost, DateTime? startDate)
             {
                 Limit = limit;
                 Offset = offset;
+                IsGoing = isGoing;
+                IsHost = isHost;
+                StartDate = startDate ?? DateTime.Now;
             }
 
             public int? Limit { get; set; }
             public int? Offset { get; set; }
+            public bool IsGoing { get; set; }
+            public bool IsHost { get; set; }
+            public DateTime? StartDate { get; set; }
         }
 
         public class Handler : IRequestHandler<Query, ActivitiesEnvelope>
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
+            private readonly IUserAccessor _userAccessor;
 
-            public Handler(DataContext context, IMapper mapper)
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
             {
                 _context = context;
                 _mapper = mapper;
+                _userAccessor = userAccessor;
             }
 
             public async Task<ActivitiesEnvelope> Handle(Query request, CancellationToken cancellationToken)
             {
                 var queryable = _context.Activities
                     .AsSingleQuery()
+                    .Where(x => x.Date >= request.StartDate)
+                    .OrderBy(x => x.Date)
                     .Include(a => a.Comments)
                     .Include(a => a.UserActivities)
                     .ThenInclude(u => u.AppUser)
                     .ThenInclude(x => x.Photos)
                     .AsQueryable();
+
+                if (request.IsGoing && !request.IsHost)
+                {
+                    queryable = queryable.Where(x => x.UserActivities
+                    .Any(a => a.AppUser.UserName == _userAccessor.GetCurrentUsername()));
+                }
+
+                if (request.IsHost && !request.IsGoing)
+                {
+                    queryable = queryable.Where(x => x.UserActivities
+                    .Any(a => a.AppUser.UserName == _userAccessor.GetCurrentUsername() && a.IsHost));
+                }
 
                 var activities = await queryable
                     .Skip(request.Offset ?? 0)
